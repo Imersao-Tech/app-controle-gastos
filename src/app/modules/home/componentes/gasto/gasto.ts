863c1307228631ffb3ea623d814a63bac2fcaffb
Alessandra Nastassja <alessandra.nastassja@hotmail.com>
2026-04-22 22:28:56 -0300

    feat: add fontawesome + icon

--- src/app/modules/home/componentes/gasto/gasto.ts
+++ src/app/modules/home/componentes/gasto/gasto.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { CurrencyMaskModule } from "ng2-currency-mask";
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faWallet } from '@fortawesome/free-solid-svg-icons';
+
+
 @Component({
   selector: 'app-gasto',
-  imports: [CurrencyMaskModule],
+  imports: [CurrencyMaskModule, FontAwesomeModule],
   standalone: true,
   templateUrl: './gasto.html',
   styleUrl: './gasto.scss',
 })
-export class Gasto {}
+export class Gasto {
+  faWallet = faWallet;
+}
