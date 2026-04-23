f0d089af91621311788cc75664f23262220ad60c
Alessandra Nastassja <alessandra.nastassja@hotmail.com>
2026-04-22 21:43:00 -0300

    feat: criacao do component de gasto

---
+++ src/app/modules/home/componentes/gasto/gasto.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gasto',
+  imports: [],
+  standalone: true,
+  templateUrl: './gasto.html',
+  styleUrl: './gasto.scss',
+})
+export class Gasto {}
